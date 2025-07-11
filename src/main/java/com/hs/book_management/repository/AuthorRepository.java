7dd540f268e9900aaa3a5e071ce7f0b16d7caac9
USER <rhs9827@naver.com>
2025-07-11 10:47:55 +0900

    first commit

---
+++ src/main/java/com/hs/book_management/repository/AuthorRepository.java
@@ -0,0 +1,45 @@
+package com.hs.book_management.repository;
+
+import com.hs.book_management.model.Author;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class AuthorRepository {
+    private final Map<Integer, Author> store = new LinkedHashMap<>();
+    private final AtomicInteger seq = new AtomicInteger(0);
+
+    public List<Author> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public Optional<Author> findById(Integer id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    public Author save(Author author) {
+        if (author.getId() == null) {
+            author.setId(seq.incrementAndGet());
+        }
+        store.put(author.getId(), author);
+
+        return author;
+    }
+
+    public Author update(Integer id, Author updatedAuthor) {
+        if(!store.containsKey(id)) {
+            throw new NoSuchElementException(id + "의 저자가 없습니다.");
+        }
+
+        updatedAuthor.setId(id);
+        store.put(id, updatedAuthor);
+
+        return updatedAuthor;
+    }
+
+    public void delete(Integer id) {
+        store.remove(id);
+    }
+}
---
+++ src/main/java/com/hs/book_management/service/AuthorService.java
@@ -0,0 +1,33 @@
+package com.hs.book_management.service;
+
+import com.hs.book_management.model.Author;
+import com.hs.book_management.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public List<Author> getAll() { return authorRepository.findAll(); }
+
+    public Author getById(Integer id) {
+        return authorRepository.findById(id).orElseThrow(() -> new NoSuchElementException("저자 없음"));
+    }
+
+    public Author create(Author author) {
+        return authorRepository.save(author);
+    }
+
+    public Author update(Integer id, Author updateAuthor) {
+        return authorRepository.update(id, updateAuthor);
+    }
+
+    public void delete(Integer id) {
+        authorRepository.delete(id);
+    }
+}
